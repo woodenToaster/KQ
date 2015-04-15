aa727b835975b7839fdc1323fff7f86ef3cad3c3
woodenToaster <litmusik@yahoo.com>
2015-04-14 20:46:17 -0500

    After much refactoring, clean code for basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
+#include "SDL.h"
+
+bool shouldClose(SDL_Event* event);
+bool windowCloseClicked(SDL_Event* event);
+bool escapePressed(SDL_Event* event);
+bool isWindowEvent(SDL_Event* event);
+bool isWindowCloseEvent(SDL_Event* event);
+bool isKeyboardEvent(SDL_Event* event);
+bool keyPressedIsEscape(SDL_Event* event);
 
 int main(int argc, char** argv) {
-  std::cout << "Kirp's Quest" << '\n';
+  
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window* window = SDL_CreateWindow("Hi", 0,0,200,100,0);
+  
+  if(!window) {
+    std::cout << "Whoops!" << '\n';
+  }                    
+
+  SDL_Event event;
+
+  bool quit = false;
+
+  while(!quit) {
+    if(SDL_PollEvent(&event)) 
+      quit = shouldClose(&event);
+  }
+
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+
   return 0;
+}
+
+bool shouldClose(SDL_Event* event) {
+    return windowCloseClicked(event) || escapePressed(event);
+}
+
+bool windowCloseClicked(SDL_Event* event) {
+  return isWindowEvent(event) && isWindowCloseEvent(event);
+}
+
+bool escapePressed(SDL_Event* event) {
+  return isKeyboardEvent(event) && keyPressedIsEscape(event);
+}
+
+bool isWindowEvent(SDL_Event* event) {
+  return event->type == SDL_WINDOWEVENT;
+}
+
+bool isWindowCloseEvent(SDL_Event* event) {
+  return event->window.event == SDL_WINDOWEVENT_CLOSE;
+}
+
+bool isKeyboardEvent(SDL_Event* event) {
+  return event->type == SDL_KEYDOWN;
+}
+
+bool keyPressedIsEscape(SDL_Event* event) {
+  return event->key.keysym.sym == SDLK_ESCAPE;
 }
